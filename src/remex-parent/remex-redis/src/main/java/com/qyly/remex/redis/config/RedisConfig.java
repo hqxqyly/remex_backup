68f89a517f16b40d9af4a693f1acdc80bd440297
hqxqyly <hqxfhlx@163.com>
2019-11-22 14:32:12 +0800

    ss

--- src/remex-parent/remex-redis/src/main/java/com/qyly/remex/redis/config/RedisConfig.java
+++ src/remex-parent/remex-redis/src/main/java/com/qyly/remex/redis/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.qyly.remex.redis.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,6 +11,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import com.qyly.remex.redis.properties.RedisProperties;
 import com.qyly.remex.utils.Assert;
 import com.qyly.remex.utils.ObjectUtils;
+import com.qyly.remex.utils.StringUtils;
+
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * redis config
@@ -45,13 +49,34 @@ public class RedisConfig {
 		JedisConnectionFactory bean = null;
 		//集群
 		if (addressArr.length > 1) {
+			//集群配置
+			RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+			for (String address : addressArr) {
+				String[] hostNameArr = address.split(":");
+				clusterConfig.clusterNode(hostNameArr[0], Integer.valueOf(hostNameArr[1]));
+			}
+			clusterConfig.setMaxRedirects(properties.getMaxRedirects());
 			
+			//连接池配置
+			JedisPoolConfig poolConfig = new JedisPoolConfig();
+			poolConfig.setMaxTotal(properties.getMaxIdle());
+			poolConfig.setMaxIdle(properties.getMaxIdle());
+			poolConfig.setMaxWaitMillis(properties.getMaxWaitMillis());
+			
+			bean = new JedisConnectionFactory(clusterConfig, poolConfig);
 		} else {  //单点
 			bean = new JedisConnectionFactory();
 			String[] hostNameArr = addressArr[0].split(":");
+			
 			bean.setHostName(hostNameArr[0]);
 			bean.setPort(Integer.valueOf(hostNameArr[1]));
+			if (StringUtils.isNotBlank(properties.getPassword())) {
+				bean.setPassword(properties.getPassword());
+			}
+			bean.setTimeout(properties.getTimeout());
 		}
+		
+		bean.setDatabase(properties.getDatabase());
 		
 		return bean;
 	}
@@ -62,10 +87,11 @@ public class RedisConfig {
 	 * @param properties
 	 * @return
 	 */
-	@Bean
+	@Bean("redisTemplate")
 	@ConditionalOnMissingBean
-	public RedisTemplate<String, Object> createRedisTemplate() {
+	public RedisTemplate<String, Object> createRedisTemplate(JedisConnectionFactory jedisConnectionFactory) {
 		RedisTemplate<String, Object> bean = new RedisTemplate<>();
+		bean.setConnectionFactory(jedisConnectionFactory);
 		bean.setDefaultSerializer(new StringRedisSerializer());
 		return bean;
 	}
--- src/remex-parent/remex-redis/src/main/java/com/qyly/remex/redis/properties/RedisProperties.java
+++ src/remex-parent/remex-redis/src/main/java/com/qyly/remex/redis/properties/RedisProperties.java
@@ -12,7 +12,28 @@ public class RedisProperties {
 	public static final String REDIS_PREFIX = "redis";
 	
 	/** redis地址，支持单点和集群，多个地址以,号隔开，例：127.0.0.1:6379, 127.0.0.1:6378 */
-	protected String[] hostName;
+	protected String[] hostName = new String[] {"localhost:6379"};
+	
+	/** 密码 */
+	protected String password;
+	
+	/** db序列 */
+	protected Integer database = 0;
+	
+	/** 超时时间，单位：毫秒 */
+	protected Integer timeout;
+	
+	/** 集群，连接池获取失败 最大重定向次数 */
+	protected Integer maxRedirects;
+	
+	/** 集群，连接池最大空闲数 */
+	protected Integer maxIdle;
+	
+	/** 集群，连接池最大连接数 */
+	protected Integer maxTotal;
+	
+	/** 集群，连接池最大阻塞等待时间，负值表示没有限制 */
+	protected Integer maxWaitMillis;
 	
 	public void setHostName(String[] hostName) {
 		this.hostName = hostName;
@@ -20,5 +41,61 @@ public class RedisProperties {
 	
 	public String[] getHostName() {
 		return hostName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(Integer database) {
+		this.database = database;
+	}
+
+	public Integer getMaxRedirects() {
+		return maxRedirects;
+	}
+
+	public void setMaxRedirects(Integer maxRedirects) {
+		this.maxRedirects = maxRedirects;
+	}
+
+	public Integer getTimeout() {
+		return timeout;
+	}
+
+	public void setTimeout(Integer timeout) {
+		this.timeout = timeout;
+	}
+
+	public Integer getMaxIdle() {
+		return maxIdle;
+	}
+
+	public void setMaxIdle(Integer maxIdle) {
+		this.maxIdle = maxIdle;
+	}
+
+	public Integer getMaxTotal() {
+		return maxTotal;
+	}
+
+	public void setMaxTotal(Integer maxTotal) {
+		this.maxTotal = maxTotal;
+	}
+	
+	public void setMaxWaitMillis(Integer maxWaitMillis) {
+		this.maxWaitMillis = maxWaitMillis;
+	}
+	
+	public Integer getMaxWaitMillis() {
+		return maxWaitMillis;
 	}
 }
